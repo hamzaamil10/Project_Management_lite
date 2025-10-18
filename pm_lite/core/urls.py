f2576e6e3d7e90771bef293b78cf5da85b9e3049
Hamza amine amil <hamza.amineamil@gmail.com>
2025-10-18 12:33:50 +0100

    feat: fixing minor issue on pointing URL in templates

--- pm_lite/core/urls.py
+++ pm_lite/core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('projects/', views.project_list, name='project-list'),
+    path('projects/', views.project_list, name='project_page'),
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
     path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
 ]
